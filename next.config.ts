f9a2001f8ba7b1985180359a2cb9c6e322065470
junesok <iloveglazeddonut@gmail.com>
2026-03-27 14:13:40 +0900

    chore: Cloudinary 도메인 next.config images 허용 등록

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,9 @@ const nextConfig: NextConfig = {
     // 홈 디렉토리에 다른 package-lock.json이 있어 발생하는 workspace root 오감지 방지
     root: path.resolve(__dirname),
   },
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
 };
 
 export default nextConfig;
